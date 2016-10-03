52276448c4050163017729852692401b77681bd9
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2016-10-03 12:36:57 -0700

    Revert "try getting past qemu crash by removing libraries with interesting platform deps"
    This reverts commit b1f8c2977d111f92167c9c0968699dc16f1f5534.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate iron;
+extern crate time;
 
 // iron
 use iron::prelude::*;
 // middleware
 use iron::{BeforeMiddleware, AfterMiddleware, typemap};
+use time::precise_time_ns;
 // mime types
 // use iron::mime::{Mime, TopLevel, SubLevel, Attr, Value};
 // headers
@@ -15,14 +17,14 @@ impl typemap::Key for ResponseTime { type Value = u64; }
 
 impl BeforeMiddleware for ResponseTime {
     fn before(&self, req: &mut Request) -> IronResult<()> {
-        req.extensions.insert::<ResponseTime>(5678);
+        req.extensions.insert::<ResponseTime>(precise_time_ns());
         Ok(())
     }
 }
 
 impl AfterMiddleware for ResponseTime {
     fn after(&self, req: &mut Request, res: Response) -> IronResult<Response> {
-        let delta = 12345 - *req.extensions.get::<ResponseTime>().unwrap();
+        let delta = precise_time_ns() - *req.extensions.get::<ResponseTime>().unwrap();
         println!("Request took: {} ms", (delta as f64) / 1000000.0);
         Ok(res)
     }
@@ -30,19 +32,14 @@ impl AfterMiddleware for ResponseTime {
 
 fn hello_world(req: &mut Request) -> IronResult<Response> {    
     // delta to check time spent
-    let delta = 23456 - *req.extensions.get::<ResponseTime>().unwrap();
+    let delta = precise_time_ns() - *req.extensions.get::<ResponseTime>().unwrap();
     let delta_ms = (delta as f64) / 1000000.0;
     
     // prepare an html string
+    let mut msg_text: String = "<html><head><title></title></head><body>So I guess bill is impressed. \
+                                But how can they make money?<br> \
+                                It took this long to get here (ms): ".to_owned();
     let msg_delta: &String = &delta_ms.to_string();
-    let mut msg_text: String = "<html>\
-                                    <head>\
-                                    <title>Rust Web ARM</title>\
-                                    </head>\
-                                    <body>\
-                                        This webserver works.<br>\
-                                        Obviously.<br>\
-                                        It took this long to get here (ms): ".to_owned();
     msg_text.push_str(msg_delta);
     msg_text.push_str("</body></html>");
     
